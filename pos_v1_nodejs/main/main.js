60a861b44af2c0b1b2d26512addcb523b02e5cf8
quinnHuang <quinn.huang@oocl.com>
2018-07-07 16:32:34 +0800

    [Quinn] 将posv1部署到NodeJS的本地服务端

---
+++ pos_v1_nodejs/main/main.js
@@ -0,0 +1,130 @@
+'use strict';
+
+const {loadAllItems,loadPromotions}=require("../spec/fixtures");
+
+function printReceipt(cart) {
+  //格式化条形码的字符串集合,转化为{barcode：，count：}
+  const barcodeObjList=formatCartBarcodeList(cart);
+  //统计商品数量  15'
+  let receiptItems=countBarcodeNum(barcodeObjList);
+  //获得购买的商品的信息  4'
+  getReceiptItemMsg(receiptItems);
+  //计算每种商品优惠后的小计，并生成收据对象
+  const receipt=calSubtotal(receiptItems);
+  //计算订单总价和节省金额  3'
+  calTotalAndSaved(receipt);
+  //生成收据内容字符串  5'
+  const receiptString=generateReceiptString(receipt);
+  //打印收据
+  console.log(receiptString);
+}
+//格式化条形码的字符串集合,转化为{barcode：，count：}
+function formatCartBarcodeList(barcodeList){
+  const barcodeObjList=[];
+  for (let barcode of barcodeList) {
+    barcodeObjList.push(formatBarcode(barcode));
+  }
+  return barcodeObjList;
+}
+//对一个购物车中的条形码进行格式化为对象
+function formatBarcode(code) {
+  let barcodeObj={barcode:code,count:1};
+  //如果存在“-”，则需要切割
+  if (code.indexOf('-')!==-1)
+    barcodeObj=spiltBarcode(code);
+  return barcodeObj;
+}
+//切割含有“-”的条形码，获得具体的条形码和数量
+function spiltBarcode(code) {
+  let index=code.indexOf('-');
+  let barcode=code.slice(0,index);
+  let count=parseFloat(code.slice(index+1));
+  return {barcode,count};
+}
+//统计商品数量
+function countBarcodeNum(barcodeObjList){
+  const receiptItems=[];
+  for (let barcodeObj of barcodeObjList) {
+    let item=findItemByBarcode(barcodeObj,receiptItems,obj=>obj.barcode)
+    if(item!==null)
+      item.count+=barcodeObj.count;
+    else
+      receiptItems.push(barcodeObj);
+  }
+  //console.info("--------------------\n");
+  //console.info(receiptItems);
+  return receiptItems;
+}
+//判断清单商品列表中是否已经存在某个条形码
+function findItemByBarcode(barcodeObj,receiptItems,getAttr){
+  for (let item of receiptItems) {
+    if (getAttr(barcodeObj)===getAttr(item))
+      return item;
+  }
+  return null;
+}
+//获得购买的商品的信息  4'
+function getReceiptItemMsg(items){
+  const allGoods=loadAllItems();
+  for (let item of items) {
+    let good=findItemByBarcode(item,allGoods,obj=>obj.barcode)
+    if (good!==null) {
+      item.name=good.name;
+      item.unit=good.unit;
+      item.price=good.price;
+    }
+  }
+  console.info(items);
+}
+//计算每种商品优惠后的小计，并生成收据对象
+function calSubtotal(items) {
+  for (let item of items) {
+    const promotions=loadPromotions();
+    item.subtotal=item.count*item.price;
+    if (isPromote(promotions,item)) {
+      item.subtotal=(item.count-Math.floor(item.count/3))*item.price;
+    }
+  }
+  const receipt={items};
+  return receipt;
+}
+//判断商品是否参与优惠活动
+function isPromote(promotions,item){
+  let promote=false;
+  for (let promotion of promotions) {
+    if(promotion.type!=="BUY_TWO_GET_ONE_FREE"){
+      continue;
+    }
+    for (let barcode of promotion.barcodes) {
+      if (item.barcode===barcode&&item.count>=3) {
+        promote=true;
+        break;
+      }
+    }
+  }
+  return promote;
+}
+//计算订单总价和节省金额
+function calTotalAndSaved(receipt) {
+  let after=0;
+  let before=0;
+  for(let item of receipt.items){
+    before+=item.count*item.price;
+    after+=item.subtotal;
+  }
+  receipt.total=after;
+  receipt.saved=before-after;
+}
+//生成收据内容字符串
+function generateReceiptString(receipt) {
+  let receiptString="***<没钱赚商店>收据***\n";
+  for (let item of receipt.items){
+    receiptString+="名称："+item.name+"，数量："+item.count+item.unit+"，单价："+(item.price).toFixed(2)+"(元)，小计："+(item.subtotal).toFixed(2)+"(元)\n";
+  }
+  receiptString+="----------------------\n";
+  receiptString+="总计："+(receipt.total).toFixed(2)+"(元)\n";
+  receiptString+="节省："+(receipt.saved).toFixed(2)+"(元)\n";
+  receiptString+="**********************";
+  return receiptString;
+}
+module.exports={calTotalAndSaved,calSubtotal,getReceiptItemMsg,generateReceiptString,countBarcodeNum,formatCartBarcodeList,printReceipt,spiltBarcode};
---
+++ pos_v1_nodejs/spec/main-spec.js
@@ -0,0 +1,148 @@
+'use strict';
+const {calTotalAndSaved,calSubtotal,getReceiptItemMsg,generateReceiptString,countBarcodeNum,formatCartBarcodeList,printReceipt,spiltBarcode}=require("../main/main");
+describe('测试条形码集合的格式化', () => {
+  it('spiltBarcode函数将“ITEM000003-2.5”形式的条形码格式化为{barcode：ITEM000003，count：2.5}的形式', () => {
+    const tag = 'ITEM000003-2.5';
+    const barcodeObj=spiltBarcode(tag);
+    const exceptObj=JSON.stringify({barcode:"ITEM000003",count:2.5});
+    expect(exceptObj).toBe(JSON.stringify(barcodeObj));
+  });
+
+  it('formatCartBarcodeList函数进行格式化后的条形码要满足{barcode：，count：}的形式', () => {
+    const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ];
+    const barcodes=formatCartBarcodeList(tags);
+    const test_index=5;
+    const expect_barcode = "ITEM000003";
+    const expect_count = 2.5;
+    expect(expect_barcode).toBe(barcodes[5].barcode);
+    expect(expect_count).toBe(barcodes[5].count);
+  });
+});
+
+describe('测试统计每种条形码的数量', () => {
+  it('countBarcodeNum函数统计每一种条形码的数量，并封装成对象集合', () => {
+    const barcodeObjItems = [
+      {barcode:"ITEM000001",count:1},
+      {barcode:"ITEM000001",count:1},
+      {barcode:"ITEM000001",count:1},
+      {barcode:"ITEM000001",count:1},
+      {barcode:"ITEM000001",count:1},
+      {barcode:"ITEM000003",count:2.5},
+      {barcode:"ITEM000005",count:1},
+      {barcode:"ITEM000005",count:2}
+    ];
+    const barcodeItems=countBarcodeNum(barcodeObjItems);
+    const test_index=2;
+    const expect_barcode = {barcode: "ITEM000005", count: 3};
+    expect(JSON.stringify(barcodeItems[test_index])).toBe(JSON.stringify(expect_barcode));
+  });
+});
+
+describe('测试丰富清单中每一种商品的信息的函数', () => {
+  it('getReceiptItemMsg函数根据所有商品的信息，完善清单中每一个商品的基本信息', () => {
+    const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ];
+    const barcodeObjList=formatCartBarcodeList(tags);
+    const receiptItems=countBarcodeNum(barcodeObjList);
+    getReceiptItemMsg(receiptItems);
+    const test_index=2;
+    const expect_receipt_item = {barcode: "ITEM000005", count: 3, name: "方便面", unit: "袋", price: 4.5};
+    expect(JSON.stringify(expect_receipt_item)).toBe(JSON.stringify(receiptItems[test_index]));
+  });
+});
+
+describe('测试计算每种商品优惠后的小计', () => {
+  it('calTotalAndSaved函数先判断商品是否满足优惠条件，再计算优惠后的小计', () => {
+    const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ];
+    const barcodeObjList=formatCartBarcodeList(tags);
+    const receiptItems=countBarcodeNum(barcodeObjList);
+    getReceiptItemMsg(receiptItems);
+    const receipt=calSubtotal(receiptItems);
+    const test_index=2;
+    const expect_subtotal = '9';
+    expect(JSON.stringify(receipt.items[test_index].subtotal)).toBe(expect_subtotal);
+  });
+});
+
+describe('测试计算订单总价和节省金额', () => {
+  it('discount函数先判断商品是否满足优惠条件，再计算优惠后的总价', () => {
+    const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ];
+    const barcodeObjList=formatCartBarcodeList(tags);
+    const receiptItems=countBarcodeNum(barcodeObjList);
+    getReceiptItemMsg(receiptItems);
+    const receipt=calSubtotal(receiptItems);
+    calTotalAndSaved(receipt);
+    const expect_tatal= 58.50;
+    const expect_saved= 7.50;
+    expect(receipt.total).toBe(expect_tatal);
+    expect(receipt.saved).toBe(expect_saved);
+
+  });
+});
+
+describe('pos', () => {
+
+  it('should print text收据内容', () => {
+
+    const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ];
+
+    spyOn(console, 'log');
+
+    printReceipt(tags);
+
+    const expectText = `***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：58.50(元)
+节省：7.50(元)
+**********************`;
+
+    expect(console.log).toHaveBeenCalledWith(expectText);
+  });
+});
